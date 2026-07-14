af46130a0eb48fe3acf35fe16459ca4dad2c475b
Bárbara <bahneko0007@gmail.com>
2026-07-13 22:38:07 -0300

    Refactor menu using functions

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from time import sleep
 
-# Criando o Menu de Interações
+# Funções do Código
+
+def cabecalho(nome, descricao):
+    print('=' * 40)
+    print(nome.center(40))
+    print(descricao.center(40))
+    print('=' * 40)
+
+def carregar_tela(nome):
+    print('⚙️CARREGANDO...'.center(40))
+    print('=' * 40)
+    sleep(1.5)
+    print(f'{nome} em desenvolvimento ⏳')
+    print('pressione ENTER para voltar ao menu...')
+    print('=' * 40)
+    input()
+
+
+# Código Principal - Criando o Menu de Interações
 
 while True:
     print('='*40)
@@ -21,59 +39,31 @@ while True:
     op = str(input('Escolha uma opção: ')).strip()
     print('='*40)
 
-# Validando se a opção tá dentro do menu de interações
+# Validando se a opção está dentro do menu de interações
 
     if op not in ['1', '2', '3', '4', '5', '6']:
         print('''Opção Inválida!
 Pressione ENTER para continuar...''')
         input()
+        continue
 
 # Iniciando as telas selecionadas no menu de interações
 
     if op == '1':
-        print('⚙️CARREGANDO...'.center(40))
-        print('=' * 40)
-        sleep(1.5)
-        print('Controle Financeiro em desenvolvimento ⏳')
-        print('pressione ENTER para voltar ao menu...')
-        print('=' * 40)
-        input()
+        cabecalho('💵CONTROLE FINANCEIRO💵', 'o seu espaço com seu dinheirinho')
+
 
     elif op == '2':
-        print('⚙️CARREGANDO...'.center(40))
-        print('=' * 40)
-        sleep(1.5)
-        print('WishList em desenvolvimento ⏳')
-        print('pressione ENTER para voltar ao menu...')
-        print('=' * 40)
-        input()
+        carregar_tela('Wishlist')
 
     elif op == '3':
-        print('⚙️CARREGANDO...'.center(40))
-        print('='*40)
-        sleep(1.5)
-        print('Skincare em desenvolvimento ⏳')
-        print('pressione ENTER para voltar ao menu...')
-        print('='*40)
-        input()
+        carregar_tela('Skincare')
 
     elif op == '4':
-        print('⚙️CARREGANDO...'.center(40))
-        print('=' * 40)
-        sleep(1.5)
-        print('Receitas de Comida em desenvolvimento ⏳')
-        print('pressione ENTER para voltar ao menu...')
-        print('=' * 40)
-        input()
+        carregar_tela('Receitas de Comida')
 
     elif op == '5':
-        print('⚙️CARREGANDO...'.center(40))
-        print('=' * 40)
-        sleep(1.5)
-        print('Metas em desenvolvimento ⏳')
-        print('pressione ENTER para voltar ao menu...')
-        print('=' * 40)
-        input()
+        carregar_tela('Metas')
 
     elif op == '6':
         print()
